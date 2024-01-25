8fb13bfc2b3364cc108528c2292b679eafab9670
rkdeofuf <asa47972000@naver.com>
2024-01-25 09:32:53 +0900

    add static to deploy with heroku

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import Header from './components/headerSection/Header'
 import Main from './components/mainSection/Main'
 import Create from './components/createSection/Create'
 import Letter from './components/Letter'
+import { Helmet } from 'react-helmet'
+import logo from './imgs/imgIntro.png'
 
 function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/create" element={<Create />} />
-                <Route path="/letter/:letterid" element={<Letter />} />
-            </Routes>
+            <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Hoons Letter</title>
+                    <meta property="og:title" content="hoons letter" />
+                    <meta property="og:image" content={logo} />
+                    <meta property="og:description" content="hoons letter kakao" />
+                </Helmet>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/letter/:letterid" element={<Letter />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     )
 }
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL = process.env.BASE_URL || import.meta.env.VITE_BASE_URL
 
 export const FONT_SIZE = {
     small: '3.7vw',
